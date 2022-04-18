281a520fd1e3d9ff451acb540dc12ff72f1e87ff
Vitoria <vitoria.elizv55@gmail.com>
2022-04-18 10:46:38 -0300

    Adiciona 5 programas de operações aritméticas básicas

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/jsprogs.js
@@ -0,0 +1,13 @@
+let a = 15
+let b = 7
+
+let adicao = a + b
+let subtracao = a - b
+let multiplicacao = a * b
+let divisao = a / b
+let modulo = a % b
+console.log(adicao)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
+console.log(modulo)
