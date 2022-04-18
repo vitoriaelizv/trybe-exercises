1dd3fd2d142381c64ac458f2b23a4534b13e59ba
Vitoria <vitoria.elizv55@gmail.com>
2022-04-18 16:36:57 -0300

    Cria programa de conversão de notas (porcentagem para letra)

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/jsprogs.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/jsprogs.js
@@ -1,23 +1,18 @@
-let pecaDeXadrez = 'CAVALO';
+let porcentagemNota = 78;
 
-switch (pecaDeXadrez.toUpperCase()) {
-    case 'CAVALO':
-        console.log('CAVALO: movimento em "L"')
-        break
-    case 'TORRE':
-        console.log('TORRE: movimento horizontal e vertical')
-        break
-    case 'BISPO':
-        console.log('BISPO: movimento diagonal')
-        break
-    case 'PEÃO':
-        console.log('PEÃO: movimento para frente')
-        break
-    case 'REI':
-        console.log('REI: movimento para qualquer direção, de uma em uma casa')
-        break
-    case 'RAINHA':
-        console.log('RAINHA: movimento horizontal, vertical e diagonal')
-    default:
-        console.log('ERRO: peça inexistente')
+if (porcentagemNota < 0 || porcentagemNota > 100) {
+    console.log("Erro: nota invalida!")
+}
+  else if (porcentagemNota >= 90) {
+    console.log('A')
+} else if (porcentagemNota >= 80) {
+    console.log('B')
+} else if (porcentagemNota >= 70) {
+    console.log('C')
+} else if (porcentagemNota >= 60) {
+    console.log('D')
+} else if (porcentagemNota >= 50) {
+    console.log('E')
+} else {
+    console.log('F')
 }
