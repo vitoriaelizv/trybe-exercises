f514f7219458fd2cfc4967a3e9106de3df676191
Vitoria <vitoria.elizv55@gmail.com>
2022-04-18 18:02:24 -0300

    Cria programa que identifica um número ímpar

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/jsprogs.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/jsprogs.js
@@ -1,8 +1,8 @@
 const a = 8
-const b = 33 
-const c = 18
+const b = 33
+const c = 77
 
-if (a % 2 === 0 || b % 2 === 0|| c % 2 === 0) {
+if (a % 2 !== 0 || b % 2 !== 0|| c !== 2 > 0) {
     console.log(true)
 } else {
     console.log(false)
