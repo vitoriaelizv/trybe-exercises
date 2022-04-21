01334a9b158aad4709052447b6406a6ba3bef21c
Vitoria <vitoria.elizv55@gmail.com>
2022-04-21 17:09:04 -0300

    cria 5 programas com operações aritméticas (1 para cada operação)

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/prog1.js
@@ -0,0 +1,10 @@
+let a = 13;
+let b = 4;
+
+adicao = a + b;
+subtracao = a - b;
+multiplicacao = a * b;
+divisao = a / b
+modulo = a % b;
+
+console.log([adicao, subtracao, multiplicacao, divisao, modulo])
