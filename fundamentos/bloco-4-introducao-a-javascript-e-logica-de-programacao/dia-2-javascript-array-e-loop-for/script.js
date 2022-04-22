9c8f835a374b268ebb8b1df7bbf142f6cc034d9c
Vitoria <vitoria.elizv55@gmail.com>
2022-04-22 20:12:02 -0300

    cria programa que soma os valores de um array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -5,3 +5,10 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let number of numbers) {
     console.log(number);
 };
+
+
+// exercício 2 - somar todos os valores de um array
+let soma = 0;
+for (let i = 0; i < numbers.length; i++) {
+    console.log(soma = soma + numbers[i]);
+};
