9fd56f2aa972ebebedbe1a2d3d8c3912945dc10f
Vitoria <vitoria.elizv55@gmail.com>
2022-04-22 20:10:45 -0300

    cria programa que percorre um array

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -0,0 +1,7 @@
+// variável usada do exercício 1 ao 7
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// exercício 1 - percorrer array e imprimir os valores
+for (let number of numbers) {
+    console.log(number);
+};
