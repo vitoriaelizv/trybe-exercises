ff01509779cc1c6a265d1c69a60d586de074f7fb
Vitoria <vitoria.elizv55@gmail.com>
2022-04-23 00:43:16 -0300

    cria programa que descobre número ímpar no array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -9,7 +9,6 @@ for (let numero of numeros) {
 
 // exercício 2 - somar todos os valores de um array
 let soma = 0;
-
 for (let i = 0; i < numeros.length; i++) {
     soma += numeros[i];
 };
@@ -18,17 +17,42 @@ console.log(soma);
 
 // exercício 3 - média aritmética 
 let soma2 = 0;
-
-for (let index = 0; index < numeros.length; index++) {
-    soma2 += numeros[index];
-    media = soma2 / numeros.length;
+for (let i = 0; i < numeros.length; i++) {
+    soma2 += numeros[i];
+    media = soma2;
 };
 console.log(media);
 
 
-// exercício 4  - exercicío 3 pt.2 com if/else
-if (media > 20) {
+// exercício 4 - exercício 3 pt.2 usando if/else
+if(media > 20) {
     console.log('valor maior que 20');
 } else {
     console.log('valor menor ou igual a 20');
 };
+
+
+// exercício 5 - maior valor de um array
+let valorMaximo = numeros[0];
+
+for (let index2 = 1; index2 < numeros.length; index2++) {
+    if (numeros[index2] > valorMaximo) {
+        valorMaximo = numeros[index2];
+    }
+};
+console.log(valorMaximo);
+
+
+// exercício 6 - descobrir valores ímpares 
+let resultado = 0;
+
+for (let index3 = 0; index3 < numeros.length; index3++) {
+    if (numeros[index3] % 2 !== 0) {
+      resultado += 1;
+    };
+}; 
+if (resultado === 0) {
+    console.log('Nenhum valor ímpar encontrado.');
+  } else {
+    console.log(resultado);
+  }
