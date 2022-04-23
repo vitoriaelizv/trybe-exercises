393ab5fcdc0ce3207b1438e412938c1da15b170b
Vitoria <vitoria.elizv55@gmail.com>
2022-04-22 21:12:21 -0300

    corrigindo o programa de soma

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,14 +1,15 @@
 // variável usada do exercício 1 ao 7
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numeros = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // exercício 1 - percorrer array e imprimir os valores
-for (let number of numbers) {
-    console.log(number);
+for (let numero of numeros) {
+    console.log(numero);
 };
 
 
 // exercício 2 - somar todos os valores de um array
 let soma = 0;
-for (let i = 0; i < numbers.length; i++) {
-    console.log(soma = soma + numbers[i]);
+for (let i = 0; i < numeros.length; i++) {
+    soma += numeros[i];
 };
+console.log(soma);
