3cd9ced4b097a1ed25cda01ac786a808ce1f147a
Vitoria <vitoria.elizv55@gmail.com>
2022-04-28 19:39:44 -0300

    algoritmo que inverte uma palavra

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -5,3 +5,14 @@ for (let index = 10; index > 0; index -= 1) {
     fatorial *= index;
  }
  console.log(fatorial);
+
+
+ // exercício 2 - algoritmo que inverte uma palavra
+ let palavra = 'tryber';
+ let palavraReversa = '';
+ 
+ for (let index = 0; index < palavra.length; index += 1) {
+   palavraReversa += word[palavra.length - 1 - index];
+ }
+ 
+ console.log(palavraReversa);
