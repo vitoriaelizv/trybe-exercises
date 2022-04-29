d1a7df0e568da6be7124a83424d39a32e6d39428
Vitoria <vitoria.elizv55@gmail.com>
2022-04-28 21:36:01 -0300

    cria algoritmos que retornar a menor e a maior palavra

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -16,3 +16,24 @@ for (let index = 0; index < palavra.length; index += 1) {
 };
  
 console.log(palavraReversa);
+
+
+// exercício 3 - algoritmos que retorna a menor e a maior palavra de um array, respectivamente
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let menorPalavra = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+    if (array[index].length < menorPalavra.length) {
+        menorPalavra = array[index];
+    };
+};
+
+let maiorPalavra = array[0];
+
+for (let index2 = 0; index2 < array.length; index2 += 1) {
+    if (array[index2].length > maiorPalavra.length) {
+        maiorPalavra = array[index2];
+    };
+};
+console.log([menorPalavra, maiorPalavra]);
