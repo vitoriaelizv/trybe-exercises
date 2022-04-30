63f51d23ec00e95ece46d2cfb01cc1bf2a3bf4c1
Vitoria <vitoria.elizv55@gmail.com>
2022-04-30 16:17:42 -0300

    imprime na tela a mensagem de boas-vindas a personagem do objeto

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -0,0 +1,15 @@
+// objeto que será usado ao longo dos exercícios
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+
+// exercício 1 - imprimir na tela um mensagem com para o personagem
+
+// notação por ponto (dot notation)
+console.log('Boas-vindas, ' + info.personagem);
+
+// notação por colchetes (bracked notation)
+console.log('Boas=vindas, ' + info['personagem']);
