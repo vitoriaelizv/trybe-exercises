535abdcaff24e2d7e228ab1bfce03b77df357200
Vitoria <vitoria.elizv55@gmail.com>
2022-04-30 18:28:27 -0300

    cria um segundo obejto e concatena seus valores  com os do primeiro

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -1,9 +1,9 @@
-// objeto que será usado ao longo dos exercícios
+// objeto que será usado nos exercícios 1 a 5
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+};
 
 
 // exercício 1 - imprimir na tela um mensagem com para o personagem
@@ -26,7 +26,28 @@ for (let key in info) {
 };
 
 
-// exercício 3 - for/in que mostra os valores das chaves do obejeto
+// exercício 4 - for/in que mostra os valores das chaves do obejeto
 for (let value in info) {
     console.log(info[value]);
-}
+};
+
+
+// exercício 5 - definir um segundo obejto e imprimir seus valores junto com os do primeiro
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorretne: "Sim"
+};
+
+for (let propriedades in info) {
+    if (
+      propriedades === "recorrente" &&
+      info[propriedades] === "Sim" &&
+      info2[propriedades] === "Sim"
+    ) {
+      console.log("Ambos recorrentes");
+    } else {
+      console.log(info[propriedades] + " e " + info2[propriedades]);
+    }
+  };
