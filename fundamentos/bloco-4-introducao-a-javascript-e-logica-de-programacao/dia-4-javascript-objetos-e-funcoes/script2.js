572ba021f6453f16e9aeba68db2614cfe5ed6f8d
Vitoria <vitoria.elizv55@gmail.com>
2022-04-30 21:03:47 -0300

    cria uma função que recebe um array e retorna o indice do maior valor

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script2.js
@@ -3,8 +3,21 @@ function verificaPalindromo(string){
     for(index in string){
       if(string[index] != string[(string.length - 1) - index]){
         return false;
-      }
-    }
+      };
+    };
     return true;
-}
+};
 // verificaPalindromo('arara')
+
+
+// exercício 2 - criar uma função que recebe um array e retorne o indice de maior valor
+function indiceDoMaior(array) {
+    let indiceMaior = 0;
+    for (indice in array) {
+        if (array[indiceMaior] < array[indice]) {
+            indiceMaior = indice;
+        };
+    };
+    return indiceMaior
+};
+// indiceDoMaior([2, 3, 6, 7, 10, 1])
