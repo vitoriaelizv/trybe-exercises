b0eecaa828997ca84116bd5c851d092a05ff0be7
Vitoria <vitoria.elizv55@gmail.com>
2022-05-06 13:28:18 -0300

    implementa uma função que adiciona um evento que, ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -155,3 +155,22 @@ function createDaysOfTheWeek() {
       newTask.className = 'task';
     };
     });
+
+// exercício 10 - Implemente uma função que adiciona um evento que, ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada
+function setDayColor() {
+  let selectedTask = document.getElementsByClassName('task selected');
+  let days = document.querySelector('#days');
+  let taskDiv = document.querySelector('.task');
+  let taskColor = taskDiv.style.backgroundColor;
+  
+  days.addEventListener('click', function(event){
+  let eventTargetColor = event.target.style.color;
+  if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
+    let color = selectedTask[0].style.backgroundColor;
+    event.target.style.color = color;
+  } else if (eventTargetColor === taskColor && selectedTask.length !== 0) {
+    event.target.style.color = 'rgb(119,119,119)';
+  }
+  });
+  };
+  setDayColor();
