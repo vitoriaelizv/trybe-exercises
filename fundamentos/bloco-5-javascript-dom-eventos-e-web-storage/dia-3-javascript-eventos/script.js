d314bd74c444e45d2a0ae3062726688c5f328995
Vitoria <vitoria.elizv55@gmail.com>
2022-05-06 13:20:34 -0300

    implementa duas funções que criam um efeito zoom

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -104,3 +104,25 @@ function createDaysOfTheWeek() {
 
     let dezFridays = [ 4, 11, 18, 25 ];
     displayFridays(dezFridays);
+
+// exercício 6 - Implemente duas funções que criem um efeito de "zoom". Ao passar o ponteiro do mouse em um dia do mês no calendário, o texto desse dia deve aumentar e, quando o ponteiro do mouse sair do dia, o texto deve retornar ao tamanho original
+    function dayMouseOver() {
+      let days = document.querySelector('#days');
+      
+      days.addEventListener('mouseover', function(event) {
+        event.target.style.fontSize = '30px';
+        event.target.style.fontWeight = '600';
+      })
+      };
+      
+      function dayMouseOut() {
+      let days = document.querySelector('#days');
+      
+      days.addEventListener('mouseout', function(event) {
+        event.target.style.fontWeight = '200';
+        event.target.style.fontSize = '20px';
+      })
+      };
+      
+      dayMouseOver();
+      dayMouseOut();
