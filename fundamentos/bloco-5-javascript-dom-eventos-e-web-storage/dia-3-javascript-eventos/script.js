1ca0bd6261945f67f20d8ddead0d3ff0a9dd13dc
Vitoria <vitoria.elizv55@gmail.com>
2022-05-06 13:09:05 -0300

    implementa uma função que adiciona ao botão Feriados um evento click que muda a cor de fundo

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -55,3 +55,22 @@ function createDaysOfTheWeek() {
       divButtonsContainer.appendChild(button);
     };
     createButton('Feriados');
+
+// exercício 3 - Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday"
+    function displayHolidays() {
+      let getHolidayButton = document.querySelector('#btn-holiday');
+      let getHolidays = document.querySelectorAll('.holiday')
+      let backgroundColor = 'rgb(238,238,238)';
+      let setNewColor = 'white';
+      
+      getHolidayButton.addEventListener('click', function() {
+      for (let index = 0; index < getHolidays.length; index += 1) {
+        if (getHolidays[index].style.backgroundColor === setNewColor) {
+          getHolidays[index].style.backgroundColor = backgroundColor;
+        } else {
+          getHolidays[index].style.backgroundColor = setNewColor;
+        };
+      };
+      });
+      };
+      displayHolidays();
