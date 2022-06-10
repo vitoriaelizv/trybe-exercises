dfb54cd3721beb5d12965184fb3c7090ad4c8dd7
Vitoria <vitoria.elizv55@gmail.com>
2022-06-10 13:44:09 -0300

    Testa o escopo das váriaveis e utiliza arrow functions

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,32 @@
+// 1 - Altere as variáveis para respeitarem o escopo em que foram declaradas
+
+// Modifique a estrutura da função para que ela seja uma arrow function;
+// Modifique as variáveis para que respeitem o escopo onde estão declaradas;
+// Modifique as concatenações para template literals.
+
+// function testingScope(escopo) {
+//     if (escopo === true) {
+//       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//       console.log(ifScope);
+//     } else {
+//       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//     console.log(ifScope + ' o que estou fazendo aqui ? :O'); | Se necessário esta linha pode ser removida.
+//   }
+
+//   testingScope(true);
+
+const testingScope = (escopo) => {
+    if (escopo === true) {
+        let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+        ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+        console.log(ifScope);
+    } else {
+        let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+        console.log(elseScope);
+    }
+}
+
+testingScope(false);
