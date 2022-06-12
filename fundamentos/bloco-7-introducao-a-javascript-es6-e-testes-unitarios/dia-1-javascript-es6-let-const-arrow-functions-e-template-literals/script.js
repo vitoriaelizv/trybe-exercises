1ecf4fdf8487cd38804c81ee5495d6d996092a6d
Vitoria <vitoria.elizv55@gmail.com>
2022-06-12 14:06:56 -0300

    cria duas funções JS com expecificações

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -88,3 +88,44 @@ const button = document.getElementById('button');
 const textToDisplay = document.getElementById('text');
 
 button.addEventListener('click', () => textToDisplay.innerHTML = clickCount += 1);
+
+// 4 - Crie duas funções JavaScript com as seguintes especificações:
+
+    // Função 1: Escreva uma função que substitua a letra 'x' em uma frase.
+        // A função deverá receber um nome por parâmetro;
+        // Declare dentro da função uma variável com o nome frase, do tipo const e com o valor igual a 'Tryber x aqui!';
+        // A função deverá retornar uma nova frase onde o x da frase 'Tryber x aqui!' seja substituído pelo nome passado por parâmetro.
+
+const subsX = (nome) => {
+    const frase = 'Tryber x aqui!';
+    const fraseArray = frase.split(' ');
+    for (let index = 0; index < fraseArray.length; index += 1) {
+        if (fraseArray[index] === 'x') {
+            fraseArray[index] = nome
+        }
+    }
+    return fraseArray.join(' ');
+}
+
+console.log(subsX('Bebeto'));
+
+    // Função 2: Escreva uma função que irá receber o retorno da Função 1 por parâmetro e retornará uma nova string.
+        // A função deverá receber o retorno da Função 1 por parâmetro;
+        // Declare dentro da função uma variável com o nome skills, do tipo const;
+        // A variável skills deverá ser um array contendo três strings com tecnologias que você já aprendeu.
+        // Concatene o valor retornado da Função 1, a frase 'Minhas três principais habilidades são:' e o valor da variável skills.
+
+
+const minhasSkills = (x) => {
+    const skills = ['HTML', 'CSS', 'JS'];
+    let resultado = `
+    ${x}
+    Minhas três principais habilidades são:`;
+    
+    for (let skill of skills) {
+        resultado = `${resultado} - ${skill}`;
+    }
+    return resultado;
+}
+
+console.log(minhasSkills(subsX('Bebeto')));
