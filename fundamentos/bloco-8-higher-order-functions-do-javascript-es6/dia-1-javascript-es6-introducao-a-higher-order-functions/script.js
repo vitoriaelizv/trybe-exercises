69cb8a1956dae0206890b710894f08b231d471d9
Vitoria <vitoria.elizv55@gmail.com>
2022-07-18 12:50:26 -0300

    exercício 3

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -30,4 +30,37 @@ const lotteryResult = (myNumber, callback) => {
     return callback(myNumber, number) ? 'Tente novamente.' : 'Parabéns, você ganhou!';
 };
 
-console.log(lotteryResult(2, numberChecker));
+// console.log(lotteryResult(2, numberChecker));
+
+// 3 - Crie uma HOF que receberá três parâmetros:
+// O primeiro será um array de respostas corretas (Gabarito);
+// O segundo será um array contendo as respostas fornecidas por uma pessoa estudante;
+// O terceiro é uma função que compara os dois arrays e então dá uma pontuação baseada nos acertos. 
+// Para isso essa função usará os seguintes critérios:
+// Uma resposta correta adiciona 1 ponto à pontuação final;
+// A ausência de uma resposta não altera a pontuação (quando for "N.A");
+// Uma resposta incorreta reduz a pontuação final em 0.5 ponto.
+// Ao final, a HOF deve retornar o total de pontos obtidos através das respostas fornecidas pela pessoa estudante. Utilize os seguintes arrays:
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const countPoints = (rightAnswers, studentAnswers, func) => {
+    score = 0;
+    for (let index = 0; index <= rightAnswers; index += 1) {
+        const funcReturn = func(rightAnswers[index], studentAnswers[index]);
+        score += funcReturn
+    }
+    return `Resultado final: ${score} pontos`;
+}
+
+const compareAnswers = (rightAnswer, studentAnswer) => {
+    if (rightAnswer === studentAnswer) {
+        return 1;
+    } if (studentAnswer === 'N.A') {
+        return 0;
+    }
+    return -0.5;
+};
+
+console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
