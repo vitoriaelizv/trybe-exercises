bbaecc956df2c6842796de1c2eb5badde4674bd6
Vitoria <vitoria.elizv55@gmail.com>
2022-07-18 14:22:02 -0300

    bônus - ex. 1

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -63,4 +63,40 @@ const compareAnswers = (rightAnswer, studentAnswer) => {
     return -0.5;
 };
 
-console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
+// console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
+
+// BÔNUS 
+
+// Parte I - Game Actions Simulator
+
+const mage = {
+    healthPoints: 130,
+    intelligence: 45,
+    mana: 125,
+    damage: undefined,
+};
+
+const warrior = {
+    healthPoints: 200,
+    strength: 30,
+    weaponDmg: 2,
+    damage: undefined,
+};
+
+const dragon = {
+    healthPoints: 350,
+    strength: 50,
+    damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+// 1 - Crie uma função que retorna o dano do dragão.
+// O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
+
+const dragonDame = () => {
+    let numRandom = Math.floor(Math.random() * (dragon.strength - 15 + 1)) + 15;
+    return numRandom;
+}
+
+console.log(dragonDame());
