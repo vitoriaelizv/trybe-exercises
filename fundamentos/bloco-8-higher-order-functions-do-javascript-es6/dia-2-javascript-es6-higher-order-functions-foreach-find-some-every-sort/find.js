f326907862f2cf735c622b3c5332a771099d052b
Vitoria <vitoria.elizv55@gmail.com>
2022-07-19 12:37:15 -0300

    exercício 2 - update

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/find.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/find.js
@@ -66,17 +66,3 @@ const books = [
 const authorBornIn1947 = () => books.find((book) => book.author.birthYear === 1947).author.name;
 
 // console.log(authorBornIn1947(books));
-
-// 2 - Retorne o nome do livro de menor nome
-
-function smallerName() {
-    let nameBook;
-    books.forEach((book) => {
-        if (!nameBook || book.name.length < nameBook.length) {
-            nameBook = book.name;
-        }
-    })
-    return nameBook;
-}
-
-console.log(smallerName())
---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/forEach.js
@@ -0,0 +1,13 @@
+// 2 - Retorne o nome do livro de menor nome
+
+function smallerName() {
+    let nameBook;
+    books.forEach((book) => {
+        if (!nameBook || book.name.length < nameBook.length) {
+            nameBook = book.name;
+        }
+    })
+    return nameBook;
+}
+
+console.log(smallerName())
