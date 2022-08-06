3cbdc7f7e63921bd0ace336ef2468de61b116074
Vitoria <vitoria.elizv55@gmail.com>
2022-08-06 00:31:11 -0300

    exercício 2

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -13,4 +13,26 @@ const getPlanet = (callback) => {
     callback(() => console.log('Returned planet: ', mars), 4000);
 };
 
-getPlanet(setTimeout);
+// getPlanet(setTimeout);
+
+
+// 2 - Suponha que você precise simular uma mensagem enviada do robô Curiosity de Marte para a Terra.
+// O Curiosity envia para a Terra a temperatura atual em Marte, gastando um tempo variável de até 5 segundos para que termine o envio.
+// Crie a função sendMarsTemperature de forma que:
+// A função sendMarsTemperature imprima no console o seguinte texto: 
+// "A temperatura de Marte é: temperaturaAtual graus celsius" onde, temperaturaAtual é o valor recebido da função getMarsTemperature; e
+// A mensagem deve ser impressa no console depois de no máximo 5 segundos.
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+    const maxTemperature = 58;
+    return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = (currentTemperature, callback) => {
+    callback(() => console.log(`A temperatura de Marte é: ${currentTemperature} graus celsius`), 5000);
+};
+
+sendMarsTemperature(getMarsTemperature(), setTimeout); // Imprime "A temperatura de Marte é: x graus celsius", por exemplo
