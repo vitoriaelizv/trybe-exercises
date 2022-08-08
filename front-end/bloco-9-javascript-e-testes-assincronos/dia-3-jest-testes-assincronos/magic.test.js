0b346095b9b4d331ed9e4f90dc5a437b832e30b4
Vitoria <vitoria.elizv55@gmail.com>
2022-08-08 12:00:28 -0300

    exercícios 1 e 2

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-3-jest-testes-assincronos/magic.test.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-3-jest-testes-assincronos/magic.test.js
@@ -2,6 +2,10 @@
 const { getMagicCard } = require('../magic');
 require('../../startest/mockSetup');
 
+const restoreFavoriteCards = () => {
+    favoriteCards.splice(4, favoriteCards.length);
+};
+
 jest.setTimeout(10000);
 
 describe('1 - Testes da função getMagicCard', () => {
@@ -29,5 +33,40 @@ describe('2 - Testando o retorno da função getMagicCard', () => {
         const response = await getMagicCard('130550');
 
         expect(response.name).toBe('Ancestor\'s Chosen');
+    });
+});
+
+describe('3 - Testes da função saveFavoriteMagicCard', () => {
+    afterEach(restoreFavoriteCards);
+
+    it('3.1 Testa se um novo card é adicionado a cada execução', async () => {
+        expect.assertions();
+        await saveFavoriteMagicCard('130553');
+
+        // Você também pode utilizar o matcher `toHaveLength`
+        // expect(favoriteCards).toHaveLength(5);
+        expect(favoriteCards.length).toBe(5);
+        expect(favoriteCards[4].name).toBe('Beacon of Immortality');
+
+        await saveFavoriteMagicCard('130554');
+
+        expect(favoriteCards.length).toBe(6);
+    });
+
+    it('3.2 Deve retornar favoriteCards contendo apenas os cards favoritos iniciais', () => {
+        expect.assertions();
+        expect(favoriteCards).toHaveLength(4);
+    });
+});
+
+describe('4 - Testes da função saveFavoriteMagicCard', () => {
+    it(`4.1 Implemente um teste que verifique que o array que você obteve com o map
+    contém todos os quatro nomes iniciais nomes`, async () => {
+        expect.assertions();
+
+        const expected = ['Ancestor\'s Chosen', 'Angel of Mercy', 'Aven Cloudchaser', 'Ballista Squad'];
+        const actual = favoriteCards.map((card) => card.name);
+
+        expect(actual).toEqual(expected);
     });
 });
