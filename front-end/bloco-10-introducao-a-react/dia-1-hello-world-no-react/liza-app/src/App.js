893c07a8e7032952fa2f71fcca7b3a5590df7db4
Vitoria <vitoria.elizv55@gmail.com>
2022-08-09 12:09:25 -0300

    cria lista de tarefas e utiliza-se da sintaxe do jsx

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/liza-app/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/liza-app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// criando a função Task
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
 
 function App() {
+  const tarefas = ['Trabalhar', 'Ir para a faculdade', 'Levar o cachorro para passear', 'Arrumar a casa'];
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <ul>
+          { Task('Estudar') }
+          { tarefas.map((tarefa) => <li>{ tarefa }</li>) }
+        </ul>
     </div>
   );
 }
